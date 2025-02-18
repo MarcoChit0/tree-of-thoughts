648d9bd011d9ac37edfbb4af23d58ecfd1e3d6be
Marco Chitolina <68135699+MarcoChit0@users.noreply.github.com>
2025-02-18 16:43:45 -0300

    add gemini model

--- README.md
+++ README.md
@@ -2,7 +2,9 @@ python -m venv .tot
 source .tot/bin/activate
 pip install --upgrade pip 
 pip install -r requirements.txt
+pip install -e . 
 
 # Adjust environment variables
 echo "export HUGGINGFACE_TOKEN=<HUGGINGFACE_TOKEN>" >> .tot/bin/activate
 echo "export OPENAI_API_KEY=<OPENAI_API_KEY>" >> .tot/bin/activate
+echo "export GEMINI_API_KEY=<GEMINI_API_KEY>" >> .tot/bin/activate
--- src/tot/models.py
+++ src/tot/models.py
@@ -2,10 +2,13 @@ import os
 from tot.tasks.base import Task
 import argparse
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
-
+import tiktoken
 import torch
 
+MAX_NUMBER_CANDIATES_SAMPLED_SINGLE_STEP = 20
+
 class Model:
+    
     def __init__(self, backend=None, temperature=0.7):
         self.backend = backend
         self.temperature = temperature
@@ -15,6 +18,16 @@ class Model:
     
     def get_usage(self):
         raise NotImplementedError("get_usage method must be implemented in derived classes")
+
+    def count_tokens(self, text):
+        try:
+            encoding = tiktoken.encoding_for_model(self.backend)
+        except Exception:
+            # Fallback to a default encoding if the model isn't recognized
+            print(f"Model {self.backend} not recognized for tokenization, using cl100k_base encoding")
+            encoding = tiktoken.get_encoding("cl100k_base")
+        return len(encoding.encode(text))
+
 
     def get_value(self, task, x, y, n_evaluate_sample, cache_value=True):
         value_prompt = task.value_prompt_wrap(x, y)
@@ -76,12 +89,14 @@ class Model:
 import openai
 import backoff
 gpt_models = ["gpt-4", "gpt-3.5-turbo", "gpt-4o"]
+
 class GPTModel(Model):
     def __init__(self, backend="gpt-4", temperature=0.7, stop=None):
         super().__init__(backend, temperature)
         self.completion_tokens = 0
         self.prompt_tokens = 0
         self.stop = stop
+        self.estimated_prompt_tokens = 0
 
         api_key = os.getenv("OPENAI_API_KEY", "")
         if api_key != "":
@@ -98,29 +113,69 @@ class GPTModel(Model):
     @backoff.on_exception(backoff.expo, openai.error.OpenAIError)
     def completions_with_backoff(self, **kwargs):
         return openai.ChatCompletion.create(**kwargs)
-    
+
     def generate(self, prompt, max_tokens=1000, n=1) -> list:
         messages = [{"role": "user", "content": prompt}]
         outputs = []
+        estimated_prompt_tokens = self.count_tokens(prompt, self.backend)
+        self.estimated_prompt_tokens = estimated_prompt_tokens
         while n > 0:
-            cnt = min(n, 20)
+            cnt = min(n, MAX_NUMBER_CANDIATES_SAMPLED_SINGLE_STEP)
             n -= cnt
-            res = self.completions_with_backoff(model=self.backend, messages=messages, temperature=self.temperature, max_tokens=max_tokens, n=cnt, stop=self.stop)
+            res = self.completions_with_backoff(model=self.backend, messages=messages, temperature=self.temperature, max_tokens=max_tokens + estimated_prompt_tokens, n=cnt, stop=self.stop)
             outputs.extend([choice.message.content for choice in res.choices])
             # log completion tokens
-            completion_tokens += res.usage.completion_tokens
-            prompt_tokens += res.usage.prompt_tokens
+            self.completion_tokens += res.usage.completion_tokens
+            self.prompt_tokens += res.usage.prompt_tokens
         return outputs
 
-    def gpt_usage(self):
-        global completion_tokens, prompt_tokens
+    def get_usage(self):
         if self.backend == "gpt-4":
-            cost = completion_tokens / 1000 * 0.06 + prompt_tokens / 1000 * 0.03
+            cost = self.completion_tokens / 1000 * 0.06 + self.prompt_tokens / 1000 * 0.03
         elif self.backend == "gpt-3.5-turbo":
-            cost = completion_tokens / 1000 * 0.002 + prompt_tokens / 1000 * 0.0015
+            cost = self.completion_tokens / 1000 * 0.002 + self.prompt_tokens / 1000 * 0.0015
         elif self.backend == "gpt-4o":
-            cost = completion_tokens / 1000 * 0.00250 + prompt_tokens / 1000 * 0.01
-        return {"completion_tokens": completion_tokens, "prompt_tokens": prompt_tokens, "cost": cost}
+            cost = self.completion_tokens / 1000 * 0.00250 + self.prompt_tokens / 1000 * 0.01
+        return {"completion_tokens": self.completion_tokens, "estimated_prompt_tokens": self.estimated_prompt_tokens, "prompt_tokens": self.prompt_tokens, "cost": cost}
+
+from google import genai
+from google.genai import types
+
+gemini_models = ["gemini-2.0-flash", "gemini-2.0-flash-lite-preview-02-05", "gemini-1.5-flash", "gemini-1.5-flash-8b", "gemini-1.5-pro"]
+
+class GeminiModel(Model):
+
+    def __init__(self, backend="gemini-1.5-flash-8b", temperature=0.7):
+        super().__init__(backend, temperature)
+        self.prompt_tokens = 0
+        self.response_tokens = 0
+        self.api_key = os.getenv("GEMINI_API_KEY")
+        if self.api_key is None:
+            raise ValueError("GEMINI_API_KEY environment variable not set")
+        self.client = genai.Client(api_key=self.api_key)
+
+    def generate(self, prompt, max_tokens=1000, n=1):
+        response = self.client.models.generate_content(
+            model=self.backend,
+            contents=prompt,
+            config=types.GenerateContentConfig(
+                max_output_tokens=max_tokens,
+                temperature=self.temperature,
+                candidate_count=n,
+            )
+        )
+        self.prompt_tokens += self.count_tokens(prompt)
+        print(response.candidates)
+        candidates_text = [candidate.content.parts[0].text for candidate in response.candidates]
+        self.response_tokens += sum(self.count_tokens(candidate) for candidate in candidates_text)
+        return candidates_text
+
+    def get_usage(self):
+        return {
+            "prompt_tokens": self.prompt_tokens,
+            "response_tokens": self.response_tokens,
+            "total_tokens": self.prompt_tokens + self.response_tokens
+        }
 
 class HuggingFaceModel(Model):
     def __init__(self, backend, path, temperature=0.7):
@@ -173,16 +228,18 @@ def add_instruct(models):
     return [f"{model}-Instruct" for model in models] + models
 
 def get_available_backends():
-    global qwen_models, llama_models, gpt_models
-    return add_instruct(qwen_models) + add_instruct(llama_models) + gpt_models
+    global qwen_models, llama_models, gpt_models, gemini_models
+    return add_instruct(qwen_models) + add_instruct(llama_models) + gpt_models + gemini_models
 
 def get_model(args: argparse.Namespace) -> Model:
-    global qwen_models, llama_models, gpt_models
-    if args.backend in ["gpt-4", "gpt-3.5-turbo", "gpt-4o"]:
+    global qwen_models, llama_models, gpt_models, gemini_models
+    if args.backend in gpt_models:
         return GPTModel(args.backend, args.temperature)
     elif args.backend in add_instruct(qwen_models):
         return QwenModel(args.backend, args.temperature)
     elif args.backend in add_instruct(llama_models):
         return LlamaModel(args.backend, args.temperature)
+    elif args.backend in gemini_models:
+        return GeminiModel(args.backend, args.temperature)
     else:
         raise ValueError(f"model {args.backend} not recognized")
